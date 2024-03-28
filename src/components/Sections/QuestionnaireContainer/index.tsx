ab6060d66fcad217b99324d1170fa3fa6f14b950
jsha64 <jorgemerino743@gmail.com>
2024-03-27 23:08:04 -0500

    Refactorizando #2

--- src/components/Sections/QuestionnaireContainer/index.tsx
+++ src/components/Sections/QuestionnaireContainer/index.tsx
@@ -8,46 +8,44 @@ import {
 import { capitalize } from '@/utils'
 
 export const QuestionnaireContainer = () => {
-  const {
-    currentTopic,
-    startQuestionnaire,
-    cancelQuestionnaire,
-    switchManualQuestionnaire,
-  } = useContext(TopicsContext)
-  return (
-    <div className="flex flex-col w-full">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold">{capitalize(currentTopic!.id)}</h1>
-        {!!currentTopic?.startedQuestionnaire ? (
-          <Button
-            type="secondary"
-            classes="max-w-[150px]"
-            onClick={() => {
-              cancelQuestionnaire!()
-            }}
-          >
-            Cancel
-          </Button>
-        ) : (
-          <Button
-            type="primary"
-            classes="max-w-[150px]"
-            onClick={() => {
-              switchManualQuestionnaire!()
-            }}
-          >
-            Upload file
-          </Button>
-        )}
-      </div>
+  const { 
+    currentTopic, 
+    startQuestionnaire, 
+    cancelQuestionnaire, 
+    switchManualQuestionnaire 
+  } = useContext(TopicsContext);
 
-      {!currentTopic?.startedQuestionnaire ? (
-        <QuestionnaireDescription />
+  const renderHeader = () => (
+    <div className="flex justify-between items-center">
+      <h1 className="text-2xl font-bold">{capitalize(currentTopic!.id)}</h1>
+      {currentTopic?.startedQuestionnaire ? (
+        <Button
+          type="secondary"
+          classes="max-w-[150px]"
+          onClick={() => cancelQuestionnaire?.()}
+        >
+          Cancel
+        </Button>
       ) : (
-        <>
-          <QuestionsContainer />
-        </>
+        <Button
+          type="primary"
+          classes="max-w-[150px]"
+          onClick={() => switchManualQuestionnaire?.()}
+        >
+          Upload file
+        </Button>
       )}
     </div>
-  )
-}
+  );
+
+  const renderContent = () => (
+    !currentTopic?.startedQuestionnaire ? <QuestionnaireDescription /> : <QuestionsContainer />
+  );
+
+  return (
+    <div className="flex flex-col w-full">
+      {renderHeader()}
+      {renderContent()}
+    </div>
+  );
+};
